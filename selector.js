240d7d4d37eace7973d009b8cd2e0074bc4f08b4
AndrewRS92 <andrew.rojas.saldias@gmail.com>
2024-05-24 16:56:27 +0200

    llamada API

---
+++ selector.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded",function(){
+    const currencySelector = document.getElementById("currencySelector");
+    const pricingElements = document.querySelectorAll(".pricingtable")
+
+    function fetchExchangeRates(currency){
+        const apiUrl = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`;
+
+        return fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => data.rates);
+    }
+
+
+
+
+
+
+})
