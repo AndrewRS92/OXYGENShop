7b553d1c9319f480a17788509d97a707076d1168
AndrewRS92 <andrew.rojas.saldias@gmail.com>
2024-05-26 20:31:04 +0200

    añadiendo estilos

--- subscribeModal.js
+++ subscribeModal.js
@@ -1,13 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
     const modal = document.getElementById("subscribeModal");
-    const closeBtn = document.querySelector(".close-btn");
+    const closeBtn = document.querySelector(".closeBtn"); 
     const form = document.getElementById("subscribeForm");
     const subscriberEmail = document.getElementById("subscriberEmail");
 
-    
     setTimeout(showModal, 5000);
 
-   
     window.addEventListener("scroll", function () {
         if ((window.scrollY + window.innerHeight) / document.body.scrollHeight >= 0.25) {
             showModal();
@@ -25,24 +23,20 @@ document.addEventListener("DOMContentLoaded", function () {
         sessionStorage.setItem("modalClosed", "true");
     }
 
-    
     closeBtn.addEventListener("click", closeModal);
 
-    
     window.addEventListener("click", function (event) {
         if (event.target == modal) {
             closeModal();
         }
     });
 
-    
     document.addEventListener("keydown", function (event) {
         if (event.key === "Escape") {
             closeModal();
         }
     });
 
-    
     form.addEventListener("submit", function (event) {
         event.preventDefault();
 
@@ -56,11 +50,10 @@ document.addEventListener("DOMContentLoaded", function () {
             },
             body: JSON.stringify(data),
         })
-            .then(response => response.json())
-            .then(data => {
-                console.log('Success:', data);
-                closeModal();
-            })
-
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+            closeModal();
+        });
     });
 });
