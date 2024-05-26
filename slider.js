2b81297c30b7e97bf2905af9092a2a417b9610ab
AndrewRS92 <andrew.rojas.saldias@gmail.com>
2024-05-27 00:08:38 +0200

    empezando logica de botones

--- slider.js
+++ slider.js
@@ -1,7 +1,33 @@
-class slider{
+class Slider{
     constructor(sliderId){
         this.slider = document.getElementById(sliderId);
+        
+        this.init();
+    }
+    
+    
+    init(){
         this.images = this.slider.querySelectorAll('img');
+        this.totalImages = this.images.length;
+        this.currentImageIndex = 0;
+   
+    }
+    
+    setupButtons() {
+        this.prevButton = this.slider.querySelector('.prevButton');
+        this.nextButton = this.slider.querySelector('.nextButton');
+        this.prevButton.addEventListener('click', () => this.prevImage());
+        this.nextButton.addEventListener('click', () => this.nextImage());
     }
 
+    prevImage(){
+
+    }
+
+    nextImage(){
+        
+    }
+
+
 }
+const slider = new slider('slider');
