6e8a8f0c0cc600795269c2de23d0ad02030c5286
AndrewRS92 <andrew.rojas.saldias@gmail.com>
2024-05-26 19:45:47 +0200

    añadadiendo estilos al modal y ocultandolo

--- subscribeModal.js
+++ subscribeModal.js
@@ -1,9 +1,21 @@
 document.addEventListener("DOMContentLoaded", function (){
 const modal = document.getElementById("subscribe");
 const closeBtn = document.querySelector(".closeBtn");
-const form = document.getElementById("subscriberEmail");
+const form = document.getElementById("subscriberform");
+const subscriberEmail = document.getElementById("subscriberEmail");
+
+setTimeout(showModal, 5000);
 
 
+function showModal(){
 
+    if (!sessionStorage.getItem("modalClosed")) {
+        modal.style.display = "block";
+    }
+}
+
+function closeModal(){
+
+}
 
 })
