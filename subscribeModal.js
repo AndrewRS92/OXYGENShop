b6754687419dfdc9982ecf22d2d882da15a69248
AndrewRS92 <andrew.rojas.saldias@gmail.com>
2024-05-26 19:28:03 +0200

    añadadiendo botn de cierre

---
+++ subscribeModal.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function (){
+const modal = document.getElementById("subscribe");
+const closeBtn = document.querySelector(".closeBtn");
+const form = document.getElementById("subscriberEmail");
+
+
+
+
+})
