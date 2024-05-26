2fc190093a37df069da2098910025f72ecbedeaf
AndrewRS92 <andrew.rojas.saldias@gmail.com>
2024-05-26 20:12:43 +0200

    añadiendo funciones de cierre

--- subscribeModal.js
+++ subscribeModal.js
@@ -1,41 +1,66 @@
-document.addEventListener("DOMContentLoaded", function (){
-const modal = document.getElementById("subscribe");
-const closeBtn = document.querySelector(".closeBtn");
-const form = document.getElementById("subscriberform");
-const subscriberEmail = document.getElementById("subscriberEmail");
+document.addEventListener("DOMContentLoaded", function () {
+    const modal = document.getElementById("subscribeModal");
+    const closeBtn = document.querySelector(".close-btn");
+    const form = document.getElementById("subscribeForm");
+    const subscriberEmail = document.getElementById("subscriberEmail");
 
-setTimeout(showModal, 5000);
+    
+    setTimeout(showModal, 5000);
 
+   
+    window.addEventListener("scroll", function () {
+        if ((window.scrollY + window.innerHeight) / document.body.scrollHeight >= 0.25) {
+            showModal();
+        }
+    });
 
-function showModal(){
-
-    if (!sessionStorage.getItem("modalClosed")) {
-        modal.style.display = "block";
+    function showModal() {
+        if (!sessionStorage.getItem("modalClosed")) {
+            modal.style.display = "block";
+        }
     }
-}
 
-function closeModal(){
+    function closeModal() {
+        modal.style.display = "none";
+        sessionStorage.setItem("modalClosed", "true");
+    }
 
-}
+    
+    closeBtn.addEventListener("click", closeModal);
 
-form.addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    const email = subscriberEmail.value;
-    const data = { email: email };
-
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log('Success:', data);
+    
+    window.addEventListener("click", function (event) {
+        if (event.target == modal) {
             closeModal();
+        }
+    });
+
+    
+    document.addEventListener("keydown", function (event) {
+        if (event.key === "Escape") {
+            closeModal();
+        }
+    });
+
+    
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const email = subscriberEmail.value;
+        const data = { email: email };
+
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
         })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+                closeModal();
+            })
 
     });
 });
