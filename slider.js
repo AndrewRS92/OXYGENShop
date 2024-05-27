8644b7dbc853a1f8f79d97da3a49fa5c4293e8b5
AndrewRS92 <andrew.rojas.saldias@gmail.com>
2024-05-27 09:48:58 +0200

    añadiendo funcionalidad auto

--- slider.js
+++ slider.js
@@ -1,21 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
     class Slider {
-        constructor(sliderId) {
+        constructor(sliderId,  intervalTime = 5000) {
+
             this.slider = document.getElementById(sliderId);
             this.currentImageIndex = 0;
             this.totalImages = 0;
             this.images = [];
             this.prevButton = null;
             this.nextButton = null;
+            this.intervalTime = intervalTime;
+            this.autoSlideInterval = null;
 
             this.init();
             this.setupButtons();
+            this.startAutoSlide();
         }
 
         init() {
             if (this.slider) {
                 this.images = Array.from(this.slider.querySelectorAll('img'));
                 this.totalImages = this.images.length;
+                this.showImage(this.currentImageIndex);
             }
         }
 
@@ -45,7 +50,14 @@ document.addEventListener('DOMContentLoaded', function() {
                 image.style.display = (i === index) ? 'block' : 'none';
             });
         }
+        startAutoSlide() {
+            this.autoSlideInterval = setInterval(() => {
+                this.nextImage();
+            }, this.intervalTime);
     }
 
-    const slider = new Slider('slider');
+
+}
+
+    const slider = new Slider('slider', 5000);
 });
